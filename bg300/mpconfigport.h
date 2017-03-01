ed18b0f6babec7b18b6f717103b1c6ac72a2f328
geko <geko@statusbits.com>
2017-02-28 16:54:48 -0800

    bg300: BG300 port (WIP)

--- bg300/mpconfigport.h
+++ bg300/mpconfigport.h
@@ -35,122 +35,122 @@
 #include "mpconfigboard.h"
 
 // memory allocation policies
-#define MICROPY_ALLOC_PATH_MAX      (128)
+#define MICROPY_ALLOC_PATH_MAX          (128)
 
 // emitters
-#define MICROPY_PERSISTENT_CODE_LOAD (1)
-#define MICROPY_EMIT_THUMB          (1)
-#define MICROPY_EMIT_INLINE_THUMB   (1)
+#define MICROPY_PERSISTENT_CODE_LOAD    (1)
+#define MICROPY_EMIT_THUMB              (1)
+#define MICROPY_EMIT_INLINE_THUMB       (1)
 
 // compiler configuration
-#define MICROPY_COMP_MODULE_CONST   (1)
+#define MICROPY_COMP_MODULE_CONST       (1)
 #define MICROPY_COMP_TRIPLE_TUPLE_ASSIGN (1)
 
 // optimisations
-#define MICROPY_OPT_COMPUTED_GOTO   (1)
+#define MICROPY_OPT_COMPUTED_GOTO       (1)
 #define MICROPY_OPT_CACHE_MAP_LOOKUP_IN_BYTECODE (0)
-#define MICROPY_OPT_MPZ_BITWISE     (1)
+#define MICROPY_OPT_MPZ_BITWISE         (1)
 
 // Python internal features
-#define MICROPY_READER_VFS          (1)
-#define MICROPY_ENABLE_GC           (1)
-#define MICROPY_ENABLE_FINALISER    (1)
-#define MICROPY_STACK_CHECK         (1)
+#define MICROPY_READER_VFS              (1)
+#define MICROPY_ENABLE_GC               (1)
+#define MICROPY_ENABLE_FINALISER        (1)
+#define MICROPY_STACK_CHECK             (1)
 #define MICROPY_ENABLE_EMERGENCY_EXCEPTION_BUF (1)
 #define MICROPY_EMERGENCY_EXCEPTION_BUF_SIZE (0)
-#define MICROPY_KBD_EXCEPTION       (1)
-#define MICROPY_HELPER_REPL         (1)
-#define MICROPY_REPL_EMACS_KEYS     (1)
-#define MICROPY_REPL_AUTO_INDENT    (1)
-#define MICROPY_LONGINT_IMPL        (MICROPY_LONGINT_IMPL_MPZ)
-#define MICROPY_ENABLE_SOURCE_LINE  (1)
-#define MICROPY_FLOAT_IMPL          (MICROPY_FLOAT_IMPL_FLOAT)
-#define MICROPY_STREAMS_NON_BLOCK   (1)
-#define MICROPY_MODULE_WEAK_LINKS   (1)
-#define MICROPY_CAN_OVERRIDE_BUILTINS (1)
-#define MICROPY_USE_INTERNAL_ERRNO  (1)
-#define MICROPY_VFS                 (1)
-#define MICROPY_VFS_FAT             (1)
+#define MICROPY_KBD_EXCEPTION           (1)
+#define MICROPY_HELPER_REPL             (1)
+#define MICROPY_REPL_EMACS_KEYS         (1)
+#define MICROPY_REPL_AUTO_INDENT        (1)
+#define MICROPY_LONGINT_IMPL            (MICROPY_LONGINT_IMPL_MPZ)
+#define MICROPY_ENABLE_SOURCE_LINE      (1)
+#define MICROPY_FLOAT_IMPL              (MICROPY_FLOAT_IMPL_FLOAT)
+#define MICROPY_STREAMS_NON_BLOCK       (1)
+#define MICROPY_MODULE_WEAK_LINKS       (1)
+#define MICROPY_CAN_OVERRIDE_BUILTINS   (1)
+#define MICROPY_USE_INTERNAL_ERRNO      (1)
+#define MICROPY_VFS                     (1)
+#define MICROPY_VFS_FAT                 (1)
 
 // control over Python builtins
-#define MICROPY_PY_FUNCTION_ATTRS   (1)
+#define MICROPY_PY_FUNCTION_ATTRS       (1)
 #define MICROPY_PY_BUILTINS_STR_UNICODE (1)
-#define MICROPY_PY_BUILTINS_STR_CENTER (1)
+#define MICROPY_PY_BUILTINS_STR_CENTER  (1)
 #define MICROPY_PY_BUILTINS_STR_PARTITION (1)
 #define MICROPY_PY_BUILTINS_STR_SPLITLINES (1)
-#define MICROPY_PY_BUILTINS_MEMORYVIEW (1)
-#define MICROPY_PY_BUILTINS_FROZENSET (1)
+#define MICROPY_PY_BUILTINS_MEMORYVIEW  (1)
+#define MICROPY_PY_BUILTINS_FROZENSET   (1)
 #define MICROPY_PY_BUILTINS_SLICE_ATTRS (1)
-#define MICROPY_PY_ALL_SPECIAL_METHODS (1)
-#define MICROPY_PY_BUILTINS_COMPILE (1)
-#define MICROPY_PY_BUILTINS_EXECFILE (1)
-#define MICROPY_PY_BUILTINS_POW3    (1)
-#define MICROPY_PY_BUILTINS_HELP    (1)
-#define MICROPY_PY_BUILTINS_HELP_TEXT stmhal_help_text
+#define MICROPY_PY_ALL_SPECIAL_METHODS  (1)
+#define MICROPY_PY_BUILTINS_COMPILE     (1)
+#define MICROPY_PY_BUILTINS_EXECFILE    (1)
+#define MICROPY_PY_BUILTINS_POW3        (1)
+#define MICROPY_PY_BUILTINS_HELP        (1)
+#define MICROPY_PY_BUILTINS_HELP_TEXT   stmhal_help_text
 #define MICROPY_PY_BUILTINS_HELP_MODULES (1)
 #define MICROPY_PY_MICROPYTHON_MEM_INFO (1)
-#define MICROPY_PY_ARRAY_SLICE_ASSIGN (1)
+#define MICROPY_PY_ARRAY_SLICE_ASSIGN   (1)
 #define MICROPY_PY_COLLECTIONS_ORDEREDDICT (1)
 #define MICROPY_PY_MATH_SPECIAL_FUNCTIONS (1)
-#define MICROPY_PY_CMATH            (1)
-#define MICROPY_PY_IO               (1)
-#define MICROPY_PY_IO_FILEIO        (1)
-#define MICROPY_PY_SYS_MAXSIZE      (1)
-#define MICROPY_PY_SYS_EXIT         (1)
-#define MICROPY_PY_SYS_STDFILES     (1)
-#define MICROPY_PY_SYS_STDIO_BUFFER (1)
-#ifndef MICROPY_PY_SYS_PLATFORM     // let boards override it if they want
-#define MICROPY_PY_SYS_PLATFORM     "pyboard"
+#define MICROPY_PY_CMATH                (1)
+#define MICROPY_PY_IO                   (1)
+#define MICROPY_PY_IO_FILEIO            (1)
+#define MICROPY_PY_SYS_MAXSIZE          (1)
+#define MICROPY_PY_SYS_EXIT             (1)
+#define MICROPY_PY_SYS_STDFILES         (1)
+#define MICROPY_PY_SYS_STDIO_BUFFER     (1)
+#ifndef MICROPY_PY_SYS_PLATFORM         // let boards override it if they want
+#define MICROPY_PY_SYS_PLATFORM         "pyboard"
 #endif
-#define MICROPY_PY_UERRNO           (1)
-#define MICROPY_PY_THREAD           (0)
-#define MICROPY_PY_THREAD_GIL       (0)
+#define MICROPY_PY_UERRNO               (1)
+#define MICROPY_PY_THREAD               (0)
+#define MICROPY_PY_THREAD_GIL           (0)
 
 // extended modules
-#define MICROPY_PY_UCTYPES          (1)
-#define MICROPY_PY_UZLIB            (1)
-#define MICROPY_PY_UJSON            (1)
-#define MICROPY_PY_URE              (1)
-#define MICROPY_PY_UHEAPQ           (1)
-#define MICROPY_PY_UHASHLIB         (1)
-#define MICROPY_PY_UBINASCII        (1)
-#define MICROPY_PY_URANDOM          (1)
-#define MICROPY_PY_URANDOM_EXTRA_FUNCS (1)
-#define MICROPY_PY_USELECT          (1)
-#define MICROPY_PY_UTIMEQ           (1)
-#define MICROPY_PY_UTIME_MP_HAL     (1)
-#define MICROPY_PY_MACHINE          (1)
-#define MICROPY_PY_MACHINE_PULSE    (1)
-#define MICROPY_PY_MACHINE_I2C      (1)
+#define MICROPY_PY_UCTYPES              (1)
+#define MICROPY_PY_UZLIB                (1)
+#define MICROPY_PY_UJSON                (1)
+#define MICROPY_PY_URE                  (1)
+#define MICROPY_PY_UHEAPQ               (1)
+#define MICROPY_PY_UHASHLIB             (1)
+#define MICROPY_PY_UBINASCII            (1)
+#define MICROPY_PY_URANDOM              (1)
+#define MICROPY_PY_URANDOM_EXTRA_FUNCS  (1)
+#define MICROPY_PY_USELECT              (1)
+#define MICROPY_PY_UTIMEQ               (1)
+#define MICROPY_PY_UTIME_MP_HAL         (1)
+#define MICROPY_PY_MACHINE              (1)
+#define MICROPY_PY_MACHINE_PULSE        (1)
+#define MICROPY_PY_MACHINE_I2C          (1)
 #define MICROPY_PY_MACHINE_I2C_MAKE_NEW machine_hard_i2c_make_new
-#define MICROPY_PY_MACHINE_SPI      (1)
-#define MICROPY_PY_MACHINE_SPI_MSB  (SPI_FIRSTBIT_MSB)
-#define MICROPY_PY_MACHINE_SPI_LSB  (SPI_FIRSTBIT_LSB)
+#define MICROPY_PY_MACHINE_SPI          (1)
+#define MICROPY_PY_MACHINE_SPI_MSB      (SPI_FIRSTBIT_MSB)
+#define MICROPY_PY_MACHINE_SPI_LSB      (SPI_FIRSTBIT_LSB)
 #define MICROPY_PY_MACHINE_SPI_MAKE_NEW machine_hard_spi_make_new
 #define MICROPY_PY_MACHINE_SPI_MIN_DELAY (0)
 #define MICROPY_PY_MACHINE_SPI_MAX_BAUDRATE (HAL_RCC_GetSysClockFreq() / 48)
-#define MICROPY_PY_FRAMEBUF         (1)
+#define MICROPY_PY_FRAMEBUF             (1)
 #ifndef MICROPY_PY_USOCKET
-#define MICROPY_PY_USOCKET          (1)
+#define MICROPY_PY_USOCKET              (1)
 #endif
 #ifndef MICROPY_PY_NETWORK
-#define MICROPY_PY_NETWORK          (1)
+#define MICROPY_PY_NETWORK              (1)
 #endif
 
 // fatfs configuration used in ffconf.h
-#define MICROPY_FATFS_ENABLE_LFN       (1)
-#define MICROPY_FATFS_LFN_CODE_PAGE    (437) /* 1=SFN/ANSI 437=LFN/U.S.(OEM) */
-#define MICROPY_FATFS_USE_LABEL        (1)
-#define MICROPY_FATFS_RPATH            (2)
-#define MICROPY_FATFS_MULTI_PARTITION  (1)
+#define MICROPY_FATFS_ENABLE_LFN        (1)
+#define MICROPY_FATFS_LFN_CODE_PAGE     (437)   // 1=SFN/ANSI 437=LFN/U.S.(OEM)
+#define MICROPY_FATFS_USE_LABEL         (1)
+#define MICROPY_FATFS_RPATH             (2)
+#define MICROPY_FATFS_MULTI_PARTITION   (1)
 
 // TODO these should be generic, not bound to fatfs
-#define mp_type_fileio fatfs_type_fileio
-#define mp_type_textio fatfs_type_textio
+#define mp_type_fileio      fatfs_type_fileio
+#define mp_type_textio      fatfs_type_textio
 
 // use vfs's functions for import stat and builtin open
-#define mp_import_stat mp_vfs_import_stat
-#define mp_builtin_open mp_vfs_open
+#define mp_import_stat      mp_vfs_import_stat
+#define mp_builtin_open     mp_vfs_open
 #define mp_builtin_open_obj mp_vfs_open_obj
 
 // extra built in names to add to the global namespace
@@ -222,11 +222,11 @@ extern const struct _mp_obj_module_t mp_module_network;
     { MP_OBJ_NEW_QSTR(MP_QSTR_stm), (mp_obj_t)&stm_module }, \
 
 #if defined(MCU_SERIES_F7)
-#define PYB_EXTI_NUM_VECTORS (24)
-#define MICROPY_HW_MAX_UART (8)
+#define PYB_EXTI_NUM_VECTORS    (24)
+#define MICROPY_HW_MAX_UART     (8)
 #else
-#define PYB_EXTI_NUM_VECTORS (23)
-#define MICROPY_HW_MAX_UART (6)
+#define PYB_EXTI_NUM_VECTORS    (23)
+#define MICROPY_HW_MAX_UART     (6)
 #endif
 
 #define MP_STATE_PORT MP_STATE_VM
@@ -268,8 +268,8 @@ extern const struct _mp_obj_module_t mp_module_network;
 
 #define MP_SSIZE_MAX (0x7fffffff)
 
-#define UINT_FMT "%u"
-#define INT_FMT "%d"
+#define UINT_FMT    "%u"
+#define INT_FMT     "%d"
 
 typedef int mp_int_t; // must be pointer size
 typedef unsigned int mp_uint_t; // must be pointer size
@@ -297,8 +297,8 @@ static inline mp_uint_t disable_irq(void) {
     return state;
 }
 
-#define MICROPY_BEGIN_ATOMIC_SECTION()     disable_irq()
-#define MICROPY_END_ATOMIC_SECTION(state)  enable_irq(state)
+#define MICROPY_BEGIN_ATOMIC_SECTION()      disable_irq()
+#define MICROPY_END_ATOMIC_SECTION(state)   enable_irq(state)
 
 #if MICROPY_PY_THREAD
 #define MICROPY_EVENT_POLL_HOOK \
@@ -312,16 +312,16 @@ static inline mp_uint_t disable_irq(void) {
         } \
     } while (0);
 #else
-#define MICROPY_EVENT_POLL_HOOK            __WFI();
+#define MICROPY_EVENT_POLL_HOOK __WFI();
 #endif
 
 // There is no classical C heap in bare-metal ports, only Python
 // garbage-collected heap. For completeness, emulate C heap via
 // GC heap. Note that MicroPython core never uses malloc() and friends,
 // so these defines are mostly to help extension module writers.
-#define malloc(n) m_malloc(n)
-#define free(p) m_free(p)
-#define realloc(p, n) m_realloc(p, n)
+#define malloc(n)       m_malloc(n)
+#define free(p)         m_free(p)
+#define realloc(p, n)   m_realloc(p, n)
 
 // see stm32f4XX_hal_conf.h USE_USB_FS & USE_USB_HS
 // at the moment only USB_FS is supported
